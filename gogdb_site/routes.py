d8916d7a23e815e269a508c97091cef2b937b21e
Gabriel Huber <huberg18@gmail.com>
2017-06-04 00:50:49 +0200

    More dynamic webassets configuration

--- gogdb_site/routes.py
+++ gogdb_site/routes.py
@@ -8,11 +8,11 @@ def includeme(config):
     config.add_route("product_info", "/product/{product}")
     config.add_route("gogdata", "/gogdata/{slug}")
 
-    config.add_static_view("static", "gogdb_site:static/", cache_max_age=3600)
-    config.add_static_view(
-        "generated", "gogdb_site:generated/", cache_max_age=604800)
-
     assets_env = config.get_webassets_env()
     asset_resolv = AssetResolver()
+    config.add_static_view(
+        "static", "gogdb_site:static/", cache_max_age=TIME_HOUR)
     assets_env.append_path(
         asset_resolv.resolve("gogdb_site:static/").abspath(), "static")
+    config.add_static_view(
+        "generated", assets_env.directory, cache_max_age=TIME_WEEK)
