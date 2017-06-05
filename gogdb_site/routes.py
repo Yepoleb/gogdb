c924507a1764be70fdf91365437dda1a31048f27
Gabriel Huber <huberg18@gmail.com>
2017-06-05 02:45:09 +0200

    Revert "Fix root route"
    This reverts commit 0559a7ecb793a69cda9f7bac74ae763fbcb838c7.

--- gogdb_site/routes.py
+++ gogdb_site/routes.py
@@ -4,7 +4,7 @@ TIME_HOUR = 3600
 TIME_WEEK = 604800
 
 def includeme(config):
-    config.add_route("index", "")
+    config.add_route("index", "/")
     config.add_route("product_list", "/products")
     config.add_route("product_info", "/product/{product}")
     config.add_route("gogdata", "/gogdata/{slug}")
