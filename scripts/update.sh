7533249cdb3b2bc0ca90f6eff9ff437ff56bfd69
Gabriel Huber <huberg18@gmail.com>
2017-06-13 04:49:12 +0200

    Add date command to update script to make logs more readable

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,6 +6,9 @@ SCRIPT_DIR="."
 PYTHON_BIN="${SCRIPT_DIR}/../env/bin/python3"
 CONFIG_FILE="${SCRIPT_DIR}/../development.ini"
 
+date
 $PYTHON_BIN "${SCRIPT_DIR}/refresh_cache.py" "$CONFIG_FILE"
 $PYTHON_BIN "${SCRIPT_DIR}/update_games.py" "$CONFIG_FILE"
 $PYTHON_BIN "${SCRIPT_DIR}/update_search.py" "$CONFIG_FILE"
+# Append newline
+echo
