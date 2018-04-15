332510e24e180427951595f1c9998b33c1e68ca4
Gabriel Huber <huberg18@gmail.com>
2018-04-15 17:34:12 +0200

    Update userscript

--- userscript/gogdb.user.js
+++ userscript/gogdb.user.js
@@ -5,7 +5,7 @@
 // @author Yepoleb
 // @license CC0
 // @namespace https://gogdb.org
-// @icon https://gogdb.org/static/img/gogdb_48x48.png
+// @icon https://www.gogdb.org/static/img/sizes/gogdb_48x48.png
 // @run-at document-end
 // @match https://www.gog.com/game/*
 // @grant none
@@ -13,12 +13,16 @@
 
 
 var button_element = document.createElement("a");
-button_element.innerHTML = '<img src="https://www.gogdb.org/static/img/gogdb_trans_15x15.png" alt="" style="height: 15px; margin-right: 0.5em">GOG Database';
+button_element.innerHTML = '<img src="https://www.gogdb.org/static/img/gogdb_trans.svg" alt="" style="height: 15px; margin-right: 0.5em">GOG Database';
 button_element.setAttribute("href", "https://www.gogdb.org/product/" + gogData.gameProductData.id);
 button_element.className = "wishlist-btn";
 button_element.setAttribute("style", "display: flex; flex-direction: row; justify-content: center; align-items: center");
 button_element.setAttribute("target", "_blank");
 
-var socials_element = document.getElementsByClassName("socials")[0];
-socials_element.parentNode.insertBefore(button_element, socials_element.nextSibling);
+var separator_element = document.createElement("div");
+separator_element.setAttribute("style", "border-top: 1px solid rgba(0,0,0,.08); margin-top: 10px;")
+
+var card_element = document.getElementsByClassName("socials")[0].parentNode;
+card_element.appendChild(separator_element);
+card_element.appendChild(button_element);
 
