78c4c43261031efd2fb0bcc33e19853139747aac
Gabriel Huber <huberg18@gmail.com>
2018-04-16 04:07:13 +0200

    Auto-enable flask-debugtoolbar in debug mode

--- gogdb/config.py
+++ gogdb/config.py
@@ -22,3 +22,11 @@ app.config.from_envvar("GOGDB_CONFIG")
 
 app.jinja_env.lstrip_blocks = app.config["JINJA_LSTRIP_BLOCKS"]
 app.jinja_env.trim_blocks = app.config["JINJA_TRIM_BLOCKS"]
+
+if app.debug:
+    try:
+        import flask_debugtoolbar
+        app.config["SECRET_KEY"] = os.urandom(24)
+        toolbar = flask_debugtoolbar.DebugToolbarExtension(app)
+    except ImportError:
+        pass
