7b30515e690fe7f1e2dea8fc17f785b374591819
Gabriel Huber <huberg18@gmail.com>
2018-04-16 06:43:17 +0200

    Fix favicon view

--- gogdb/views/favicon.py
+++ gogdb/views/favicon.py
@@ -9,4 +9,4 @@ from gogdb import app
 def favicon():
     static_path = os.path.join(app.root_path, "static")
     return flask.send_from_directory(
-        static_path, "favicon.ico", mimetype="image/vnd.microsoft.icon")
+        static_path, "img/favicon.ico", mimetype="image/vnd.microsoft.icon")
