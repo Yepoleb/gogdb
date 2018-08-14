cae3d9818b68cc240374fb889149dc38d08229b6
Gabriel Huber <huberg18@gmail.com>
2018-08-15 00:24:56 +0200

    Fix example configs

--- example-development.py
+++ example-development.py
@@ -1,6 +1,7 @@
 DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = "postgresql://gogdb:12345678@localhost:5432/gogdb"
+TOKEN_PATH = "/etc/gogdb/token.json"
 
 JINJA_LSTRIP_BLOCKS = True
 JINJA_TRIM_BLOCKS = True
--- example-production.py
+++ example-production.py
@@ -1,1 +1,2 @@
 SQLALCHEMY_DATABASE_URI = "postgresql://gogdb:12345678@localhost:5432/gogdb"
+TOKEN_PATH = "/etc/gogdb/token.json"
