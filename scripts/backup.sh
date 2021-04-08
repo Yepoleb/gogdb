1a9223ac3c4ecae89c4bdece6be34c409308a894
Gabriel Huber <mail@gabrielhuber.at>
2021-04-08 06:05:27 +0200

    Rewrite backup script

---
+++ scripts/backup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+STORAGE_PATH="/var/lib/gogdb/storage"
+BACKUP_PATH="/var/lib/gogdb/backups"
+
+set -e
+
+month=`date '+%Y-%m'`
+today=`date '+%Y-%m-%d'`
+
+products_path="$BACKUP_PATH/products"
+
+mkdir -p "$products_path/$month"
+cd "$STORAGE_PATH"
+echo "Compressing products"
+tar --create --xz --file "$products_path/$month/gogdb_$today.tar.xz" "products"
+cd "$products_path"
+find . -name 'gogdb_*' -printf '%P\n' | sort > "$products_path/filelist.txt"
+
+mkdir -p "$BACKUP_PATH/manifests"
+cd "$STORAGE_PATH"
+echo "Compressing manifests"
+tar --create --file "$BACKUP_PATH/manifests/manifests_current.tar" "manifests_v1" "manifests_v2"
+
