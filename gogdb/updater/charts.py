2906926db00fb46bb4134a275e7e4e65589a11f6
Gabriel Huber <mail@gabrielhuber.at>
2022-08-05 05:37:39 +0200

    Improved charts labeling

--- gogdb/updater/charts.py
+++ gogdb/updater/charts.py
@@ -9,6 +9,53 @@ import aiofiles
 from gogdb.updater.charts_css import CHARTS_CSS
 
 
+def calculate_y_scale(min_val, max_val, steps_pref):
+    """
+    Keep trying the step values in step_pref in order to see if they divide without remainder.
+    If none of them does, increase max_val by 1 and try again.
+    """
+    # Rounding to integer values because prices are decimal and rounding makes nicer scales
+    min_int = int(math.floor(min_val))
+    max_int = int(math.ceil(max_val))
+    if max_int == 0:
+        max_int = 1
+    found_steps = None
+    while True:
+        diff = max_int - min_int
+        for steps in steps_pref:
+            if diff % steps == 0:
+                found_steps = steps
+                break
+        if found_steps is not None:
+            break
+        else:
+            max_int += 1
+    return list(range(min_int, max_int + 1, diff // found_steps))
+
+def calculate_x_scale(first_day, last_day, max_steps):
+    """
+    Get a scale using only months with consistent step size.
+    Keeps increasing the step size by 1 until the number of months is lower than max_steps
+    """
+    first_month = first_day.year * 12 + first_day.month - 1  # subtract 1 to make month zero indexed
+    last_month = last_day.year * 12 + last_day.month - 1 + 1
+    num_months = last_month - first_month
+    step_size = 1
+    while num_months // step_size > 10:
+        step_size += 1
+    scale = []
+    for month_num in range(first_month, last_month + 1, step_size):
+        scale.append(datetime.date(month_num // 12, month_num % 12 + 1, 1))
+    return scale
+
+def date_to_timestamp(date):
+    return datetime.datetime.combine(date, datetime.time(0, 0, 0), tzinfo=datetime.timezone.utc) \
+        .timestamp()
+
+def format_date_timestamp(ts):
+    return datetime.datetime.fromtimestamp(ts, tz=datetime.timezone.utc).strftime("%Y-%m-%d")
+
+
 class ChartsProcessor:
     wants = {"prices"}
 
@@ -40,22 +87,28 @@ class ChartsProcessor:
         now_ts = datetime.datetime.now(datetime.timezone.utc).date()
         dataseries_y.append(dataseries_y[-1])
         dataseries_x.append(now_ts)
-        dataseries = list(zip(dataseries_x, dataseries_y))
+        dataseries_x_ts = [date_to_timestamp(x) for x in dataseries_x]
+        dataseries = list(zip(dataseries_x_ts, dataseries_y))
         max_price = max(filter(lambda x: x is not None, dataseries_y))
-        range_top = int(math.ceil(max_price / 10) * 10)
 
-        chart = pygal.DateLine()
+        y_scale = calculate_y_scale(0, max_price, steps_pref=[5, 4, 6])
+        x_scale = calculate_x_scale(dataseries_x[0], dataseries_x[-1], max_steps=10)
+        x_scale_ts = [date_to_timestamp(d) for d in x_scale]
+
+        chart = pygal.XY()
         chart.width = 1000
         chart.height = 300
         chart.show_legend = False
-        chart.range = (0, range_top)  # Round to nearest multiple of 10
-        chart.max_scale = 6
+        chart.range = (0, y_scale[-1])
         chart.include_x_axis = True
         chart.value_formatter = lambda p: "${:.2f}".format(p)
+        chart.x_value_formatter = format_date_timestamp
         chart.dots_size = 3
         chart.show_x_guides = True
         chart.js = []
         chart.css = []
+        chart.y_labels = y_scale
+        chart.x_labels = x_scale_ts
         chart.add("Price", dataseries, allow_interruptions=True)
 
         chart_etree = chart.render_tree()
