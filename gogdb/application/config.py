9a244cf22187f829b7ebc5426016ad857731f7bd
Gabriel Huber <mail@gabrielhuber.at>
2022-08-05 23:30:58 +0200

    Add CSP header in quart instead of apache

--- gogdb/application/config.py
+++ gogdb/application/config.py
@@ -1,6 +1,12 @@
 import os
 
 
+async def add_security_headers(response):
+    if "Content-Security-Policy" not in response.headers:
+        response.headers["Content-Security-Policy"] = \
+            "default-src 'self'; img-src 'self' images.gog.com;"
+    return response
+
 def configure_app(app):
     app.config["JINJA_LSTRIP_BLOCKS"] = False
     app.config["JINJA_TRIM_BLOCKS"] = False
@@ -9,3 +15,5 @@ def configure_app(app):
 
     app.jinja_env.lstrip_blocks = app.config["JINJA_LSTRIP_BLOCKS"]
     app.jinja_env.trim_blocks = app.config["JINJA_TRIM_BLOCKS"]
+
+    app.after_request(add_security_headers)
