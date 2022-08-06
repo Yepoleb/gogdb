1fcd2f9249aea4b3d8ba114fe961fde27d824194
Gabriel Huber <mail@gabrielhuber.at>
2022-08-06 04:50:08 +0200

    Fix svg inline css not being loaded because of CSP

--- gogdb/views/charts.py
+++ gogdb/views/charts.py
@@ -10,4 +10,7 @@ async def charts(prod_id):
     filename = f"{prod_id}.svg.gz"
     response = await quart.send_from_directory(charts_path, filename, mimetype="image/svg+xml")
     response.headers["Content-Encoding"] = "gzip"
+    # This is needed because otherwise the inline css in the svg would be blocked
+    response.headers["Content-Security-Policy"] = \
+        "default-src 'self'; img-src 'self'; style-src 'unsafe-inline';"
     return response
